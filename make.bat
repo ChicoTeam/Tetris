e3a9e3818f45bb21ef081b392ff63fcec436a971
Russ <russ@russ-laptop.(none)>
2011-05-26 15:10:52 -0700

    modified:   make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-g++ main.cpp board.cpp piece.cpp -lsfml-graphics -lsfml-window -lsfml-system -o Tetris.exe
+g++ main.cpp tetris.cpp board.cpp piece.cpp -lsfml-graphics -lsfml-window -lsfml-system -o Tetris.exe
